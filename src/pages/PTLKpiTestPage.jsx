f30892e90ec5bbd0ba13b9ca3fb0e09db9b94ae0
chetanft <chetan.kummari@freighttiger.com>
2025-04-17 08:50:39 +0530

    Fix AppHeader import in PTL KPI test page

--- src/pages/PTLKpiTestPage.jsx
+++ src/pages/PTLKpiTestPage.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { Box, Typography, Paper, Container } from '@mui/material';
+import { Box, Typography, Paper, Container, AppBar, Toolbar } from '@mui/material';
 import { planningKpiData, preDispatchKpiData, inTransitKpiData, postDeliveryKpiData } from '../data/ptl';
-import AppHeader from '../components/common/AppHeader';
 
 const PTLKpiTestPage = () => {
   return (
     <Box>
-      <AppHeader />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">PTL KPI Test Page</Typography>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="lg" sx={{ mt: 4 }}>
         <Typography variant="h4" gutterBottom>PTL KPI Test Page</Typography>
-        
+
         <Paper sx={{ p: 3, mb: 3 }}>
           <Typography variant="h5" gutterBottom>Planning KPIs</Typography>
           <pre>{JSON.stringify(planningKpiData, null, 2)}</pre>
         </Paper>
-        
+
         <Paper sx={{ p: 3, mb: 3 }}>
           <Typography variant="h5" gutterBottom>Pre-Dispatch KPIs</Typography>
           <pre>{JSON.stringify(preDispatchKpiData, null, 2)}</pre>
         </Paper>
-        
+
         <Paper sx={{ p: 3, mb: 3 }}>
           <Typography variant="h5" gutterBottom>In-Transit KPIs</Typography>
           <pre>{JSON.stringify(inTransitKpiData, null, 2)}</pre>
         </Paper>
-        
+
         <Paper sx={{ p: 3, mb: 3 }}>
           <Typography variant="h5" gutterBottom>Post-Delivery KPIs</Typography>
           <pre>{JSON.stringify(postDeliveryKpiData, null, 2)}</pre>
